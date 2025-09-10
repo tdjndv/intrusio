3e3f8c96727d4e0eccc8c1916c92ad97fccfffca
TengDa Jiang <tdjndv@gmail.com>
2025-09-10 18:22:48 +0800

    ads

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5274697787131519"
+     crossOrigin="anonymous"></script>
+     </head>
       <body>
         {children}
         <Analytics />
